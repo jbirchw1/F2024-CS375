55dc302aaa065a2d31e3e19ba1f873198ef5ad05
jbirchw1 <jbirchw1@binghamton.edu>
2024-09-11 10:28:16 -0400

    p3

---
+++ program3/src/main.rs
@@ -0,0 +1,17 @@
+/**
+ * John Birchwood, bona fide Rustacean
+ * jbirchw1@binghamton.edu
+ * Program 3 - Merge Sort
+ */
+
+// TODO: Make testable
+
+// TODO: heap sort
+//  - will need an insert function, which should call
+//    percolate up. Will also need to call insert from
+//    build max heap function. Will also need to call
+//    a pecolate down function from extract max function.
+
+fn main() {
+    println!("Hello, world!");
+}
