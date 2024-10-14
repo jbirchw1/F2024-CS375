b34625bc16c9a4098824141c6e36585186ddfba3
jbirchw1 <jbirchw1@binghamton.edu>
2024-10-14 01:38:44 -0400

    fully functional

--- program5/src/bfs/mod.rs
+++ program5/src/bfs/mod.rs
@@ -1,6 +1,10 @@
+use std::collections::{HashMap, VecDeque};
 use std::io::{self, BufRead};
-use std::collections::{HashMap, HashSet, VecDeque};
 
+/**
+ * Small helper function to print each vertex and adjacency list
+ * Takes graph (hashmap) as input
+ */
 pub fn print_graph(graph: &HashMap<i32, Vec<i32>>) {
     println!("adjacency list for each edge:");
     for (node, neighbors) in graph {
@@ -8,6 +12,13 @@ pub fn print_graph(graph: &HashMap<i32, Vec<i32>>) {
     }
 }
 
+/**
+ * Parse the input and create the hashmap representation of the graph
+ * creates a hashmap with each entry corresponding to a vertex, each
+ * emtry associated with an adjacency list vector
+ * returns the hasmap representation of the graph with i32 keys
+ */
+// TODO: enhance graph to be able to handle type <T> keys 
 pub fn build_graph_from_stdin() -> HashMap<i32, Vec<i32>> {
     let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
     let stdin = io::stdin();
@@ -21,9 +32,58 @@ pub fn build_graph_from_stdin() -> HashMap<i32, Vec<i32>> {
         let u: i32 = edge.next().unwrap().parse().unwrap();
         let v: i32 = edge.next().unwrap().parse().unwrap();
 
-        // Insert the directed edge from u to v (u -> v)
+        // if u exists in the hash map, insert v into its' vector
+        // if not, create u and then add v
         graph.entry(u).or_insert_with(Vec::new).push(v);
     }
 
     graph // Return ownership of the graph
+}
+
+/**
+ * Breadth first search algorithm
+ * Visits each vertex n distance away from the root until destination
+ * is found or frontier queue is empty
+ * Takes in the adjacency list graph as input and returns an option of distance
+ */
+pub fn breadth_first_search(
+    start: i32,
+    destination: i32,
+    graph: &HashMap<i32, Vec<i32>>,
+) -> Option<i32> {
+    let mut visited = HashMap::new(); // visited nodes
+    let mut distance = HashMap::new(); // distances from start node
+    let mut queue = VecDeque::new(); // frontier queue
+
+    // mark start as visited, set its distance to 0, and enqueue it
+    visited.insert(start, true);
+    distance.insert(start, 0);
+    queue.push_back(start);
+
+    // while the frontier queue is not empty
+    while let Some(node) = queue.pop_front() {
+        // If we reach the destination, return the distance
+        if node == destination {
+            return distance.get(&node).cloned(); // Return distance to destination
+        }
+
+        // use references instead of copies to avoid issues with the borrow checker
+        // NOTE: no need to update distances since each node will be discovered at its shortest
+        // distance when added to the frontier
+        // i.e., each node within n of the root is discovered on the nth pass
+        if let Some(neighbors) = graph.get(&node) {
+            for &neighbor in neighbors {
+                if let std::collections::hash_map::Entry::Vacant(e) = visited.entry(neighbor) {
+                    // mark neighbor as visited, enqueue it, and update its distance
+                    e.insert(true);
+                    distance.insert(neighbor, distance[&node] + 1);
+                    queue.push_back(neighbor);
+                }
+            }
+        }
+    }
+
+    None // If destination is not reachable, return None 
+    // this is why we return Option<i32>, in Rust this allows us
+    // to return "Null"
 }
--- program5/src/main.rs
+++ program5/src/main.rs
@@ -1,6 +1,37 @@
+use std::env;
+
 mod bfs;
 
 fn main() {
-    let mut graph = bfs::build_graph_from_stdin();
-    bfs::print_graph(&graph);
+    let debug = false;
+
+    // create graph from input
+    let graph = bfs::build_graph_from_stdin();
+    // print graph if wanted
+    if debug {
+        bfs::print_graph(&graph);
+    }
+
+    // get start and end vertices from command line
+    let args: Vec<String> = env::args().skip(1).collect();
+    let start: i32 = match args[0].parse::<i32>() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("{}", e);
+            std::process::exit(0); // Force quit program. Will not behave as expected if it continues
+        }
+    };
+    let end: i32 = match args[1].parse::<i32>() {
+        Ok(val) => val,
+        Err(e) => {
+            println!("{}", e);
+            std::process::exit(0);
+        }
+    };
+
+    // print correct output based on return value of bfs algorithm
+    match bfs::breadth_first_search(start, end, &graph) {
+        Some(dist) => println!("{}", dist),
+        None => println!("not connected"),
+    }
 }
