c0079cbb8145a93e4e9be5be239a807a6450e8b8
jbirchw1 <jbirchw1@binghamton.edu>
2024-11-01 17:05:23 -0400

    updating

--- program7/src/main.rs
+++ program7/src/main.rs
@@ -24,32 +24,10 @@ mod prim;
 /// ```
 /// let (start, end) = parse_args();
 /// ```
-fn parse_args() -> (i32, i32) {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() < 1 {
-        println!("ID-10-t Error: Not enough indices entered. Please enter start and end vertices.");
-        std::process::exit(0);
-    }
-    let start: i32 = match args[0].parse::<i32>() {
-        Ok(val) => val,
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(0); // Force quit program. Will not behave as expected if it continues
-        }
-    };
-    let end: i32 = match args[1].parse::<i32>() {
-        Ok(val) => val,
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(0);
-        }
-    };
-    (start, end)
-}
 
 /// Driver code
 fn main() {
-    let (start, end) = parse_args();
-    let my_graph = prim::build_weighted_directed_graph_from_stdin();
-    prim::print_shortest_path(start, end, &my_graph);
+    let my_graph = prim::build_weighted_undirected_graph_from_stdin();
+    prim::determine_minimum_spanning_tree_cost(&my_graph);
+    
 }
--- program7/src/prim/mod.rs
+++ program7/src/prim/mod.rs
@@ -25,7 +25,7 @@ mod priority_queue;
 /// # Time Complexity
 /// <div class="warning">This function operates in 
 /// O(V) time complexity.</div>
-pub fn build_weighted_directed_graph_from_stdin() -> HashMap<i32, Vec<(i32, f64)>> {
+pub fn build_weighted_undirected_graph_from_stdin() -> HashMap<i32, Vec<(i32, f64)>> {
     let mut graph: HashMap<i32, Vec<(i32, f64)>> = HashMap::new();
     let stdin = io::stdin();
 
@@ -42,163 +42,55 @@ pub fn build_weighted_directed_graph_from_stdin() -> HashMap<i32, Vec<(i32, f64)
         // if u exists in the hash map, insert v into its' vector
         // if not, create u and then add v
         graph.entry(u).or_insert_with(Vec::new).push((v, dist));
+        // also, add the opposite edge
+        // graph.entry(v).or_insert_with(Vec::new).push((u, dist));
     }
 
     graph // Return ownership of the graph
 }
 
-/// Prints the shortest path from a start vertex to an end vertex over a 
-/// given graph.
-/// 
-/// Start and end vertices are to be passed as i32, and the graph is to be passed
-/// as a reference to a weighted graph hashmap representation, as returned from build_weighted_directed_graph_from_stdin().
-/// 
-/// If a path is not found between start and end vertices, it will print "not connected"
-/// If a path is found, it will print the distance followed by the shortest path.
-/// E.g.
-/// ```
-/// Distance: 14
-/// 1 -> 2 -> 3 -> 4 -> 5
-/// ```
-/// 
-/// # Examples
-/// ```
-/// mod prim;
-/// 
-/// let (source, destination) = (0, 5)
-/// 
-/// let my_graph = prim::build_weighted_directed_graph_from_stdin();
-/// prim::print_shortest_path(source, destination, &my_graph);
-/// ```
-/// 
-/// # Time Complexity
-/// <div class="warning">This function operates in 
-/// O(VlogV) time complexity.</div>
-/// Note that this is only due to the call to prim(). 
-/// Otherwise, the function operates in O(V) time.
-pub fn print_shortest_path(
-    start: i32,
-    destination: i32,
+pub fn determine_minimum_spanning_tree_cost(
     graph: &HashMap<i32, Vec<(i32, f64)>> ) {
 
-    // take care of the O(1) case in which the start and end 
-    // vertices are the same.
-    if start == destination {
-        println!("Distance: 0");
-        println!("Path:  {}", start);
-        return;
-    }
-
     // get distances and predeccessors from Prim's algorithm
-    let (distances, predecessors) = prim(graph, start);
+    let minimum_spanning_tree = prim(graph);
 
-    // if node was not discovered
-    if distances.get(&destination).copied().unwrap_or(f64::INFINITY) == f64::INFINITY {
-        println!("not connected");
-        return;
+    let mut total_distance : f64 = 0.0;
+    for (key, value) in minimum_spanning_tree {
+        println!("{} / {}", key, value);
+        total_distance += value;
     }
-
-    // get distance
-    let distance_to_destination = distances.get(&destination).copied().unwrap();
-    println!("Distance: {}", distance_to_destination);
-
-    // print path by traversing predeccessors in reverse order
-    let mut stack = Vec::new();
-    stack.push(destination);
-    let mut prior = predecessors.get(&destination).copied().unwrap(); 
-    loop {
-        stack.push(prior);
-        if prior == start {
-            break;
-        }
-        prior = predecessors.get(&prior).copied().unwrap(); 
-    }
-
-    print!("Path:  ");
-    while stack.len() > 0 {
-        let curr = stack.pop();
-        print!("{} ", curr.unwrap());
-    }
-    println!("");
 }
 
-/// Prim's algorithm for all shortest paths from a start vertex.
-/// 
-/// Reads a reference to a hashmap weighted graph representation (as 
-/// described in build_weighted_directed_graph_from_stdin()) and 
-/// returns the shortest distance to all vertices from the start vertex (second argument).
-/// 
-/// Returns 2 HashMaps: one for distances and another for each vertex's predecessor.
-/// This function can be called on its own, or more typically from inside the print_shortest_path function.
-/// 
-/// # Examples
-/// ```
-/// mod prim;
-/// 
-/// let source = 0;
-/// 
-/// let my_graph = prim::build_weighted_directed_graph_from_stdin();
-/// let distances, predecessors = prim::prim(&my_graph, source);
-/// ``` 
-/// 
-/// # Time Complexity
-/// <div class="warning">This function operates in 
-/// O(VlogV) time complexity.</div>
 pub fn prim(
-    graph: &HashMap<i32, 
-    Vec<(i32, f64)>>, 
-    start: i32,
-) -> (HashMap<i32, f64>, HashMap<i32, i32>) {
+    graph: &HashMap<i32, Vec<(i32, f64)>>
+) -> HashMap<i32, f64> {
+    let mut mst: HashMap<i32, f64> = HashMap::new();  // Minimum edge weight to the MST
+    let mut pq = priority_queue::PriorityQueue::new();  // Priority queue to select the next minimum edge
 
-    let mut distances: HashMap<i32, f64> = HashMap::new();  // Stores the shortest distance to each node
-    let mut predecessors: HashMap<i32, i32> = HashMap::new();  // Stores the predeccessor for each node
+    // Start with an arbitrary node (e.g., node 0)
+    let start_node = 0;
+    mst.insert(start_node, 0.0);
+    pq.insert_with_priority(start_node, 0.0);
 
-    // Initialize the priority queue
-    let mut pq = priority_queue::PriorityQueue::new();
-
-    // Initialize distances to infinity and start node to 0
-    for &vertex in graph.keys() {
-        distances.insert(vertex, f64::INFINITY);
-        pq.insert_with_priority(vertex, f64::INFINITY);
-        // little hack-y, but allows you to add nodes that aren't nececcarily "u" in a directed edge
-        let destinations = graph.get(&vertex).unwrap();
-        for d in destinations {
-            distances.insert(d.0, f64::INFINITY);
-            // dont need to insert to pq here since any vertex that does not have
-            // an edge start will not have any neighbors
-        }
-
-    }
-
-    // Start node with distance 0
-    distances.insert(start, 0.0);
-    pq.insert_with_priority(start, 0.0);  
-
-    // THIS is Prim's algorithm
+    // THIS is Dijkstra's algorithm
     // pop lowest distance off priority queue
+    // TODO: Update loop to stop when MST contains same amount of vertices as graph (not in terms of edges, that is)
     while let Some(current) = pq.pop() {
+        // add current to mst
+        mst.insert(current.get_key(), current.get_distance());
         // iterate through each neighbor, if it exists
         if let Some(neighbors) = graph.get(&current.get_key()) {
             for &neighbor in neighbors {
-                // calculate old and new distances
-                let prior = distances.get(&neighbor.0).copied().unwrap();
-                let post = &current.get_distance() + &neighbor.1;
-                if prior > post { // if a better distance has been discovered
-                    // update distance map, predeccessor map, and relax edge in priority queue (or add it)
-                    distances.insert(neighbor.0, post);
-                    predecessors.insert(neighbor.0, current.get_key());
-                    if pq.contains_key(neighbor.0) {
-                        pq.relax(neighbor.0, post);
-                    }
-                    else {
-                        pq.insert_with_priority(neighbor.0, post);
-                    }
-                    
+                if !mst.contains_key(&neighbor.0) {
+                    // if the mst does not yet contain the vertex associated with the tail end of the edge
+                    // add it to the priority queue
+                    pq.insert_with_priority(neighbor.0, neighbor.1);
                 }
             }
         }
     }
 
-    // return distances and each vertex's predecessor
-    (distances, predecessors)
+    // Return the mst, with distances for each vertex
+    mst
 }
