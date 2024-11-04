a346b081970c193fd5dae5d6f987db3f1a122fc8
jbirchw1 <jbirchw1@binghamton.edu>
2024-11-04 10:21:52 -0500

    init p8

---
+++ program8/src/main.rs
@@ -0,0 +1,7 @@
+//! Binghamton University - Fall 2024 Design and Analysis of Algorithms
+//! 
+//! Program 8: Longest Common Substring  - John Birchwood
+
+fn main() {
+    println!("Hello, world!");
+}
